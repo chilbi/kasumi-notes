467e1d59b532f2b31b752a8bf16af40b0e4ee26c
chilbi <13232158717@163.com>
2020-10-11 05:18:35 +0800

    Updates

---
+++ precache-manifest.d259dc7344d0eed1be9cf07bfe0d2b18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d0fb585838eb542553be4101add0c4",
+    "url": "/kasumi-notes/index.html"
+  },
+  {
+    "revision": "998426560964917342d2",
+    "url": "/kasumi-notes/static/js/3.a541a84c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kasumi-notes/static/js/3.a541a84c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fe4dcc4b29ba8426607",
+    "url": "/kasumi-notes/static/js/data.00903c28.chunk.js"
+  },
+  {
+    "revision": "339ef8fd0b6d2c80efdc",
+    "url": "/kasumi-notes/static/js/main.0cf61023.chunk.js"
+  },
+  {
+    "revision": "3762caefa79941136d31",
+    "url": "/kasumi-notes/static/js/runtime-main.49dc9cb6.js"
+  },
+  {
+    "revision": "a4bfd91ddbed8c62811499df6ef164ae",
+    "url": "/kasumi-notes/static/media/FOT-UDMarugo_LargePro-B.a4bfd91d.otf"
+  }
+]);
